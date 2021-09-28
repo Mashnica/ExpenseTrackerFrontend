0174f282097f23190ec9b53fc4cf979a2e85b8bc
masa.micunovic <masa.micunovic@vega.local>
2021-09-28 13:49:37 +0200

    Axios settings

--- expense-tracker-app/src/pages/GroupListPage.js
+++ expense-tracker-app/src/pages/GroupListPage.js
@@ -1,53 +1,72 @@
 import * as React from "react";
-import MyTable from "../componets/MyTable";
-import Button from "@mui/material/Button";
-import styles from "../styles/Navbar.module.css";
-import AddGroupExpenseIncomeDialog from "../componets/AddGroupExpenseIncomeDialog";
+// import MyTable from "../componets/MyTable";
+// import Button from "@mui/material/Button";
+// import styles from "../styles/Navbar.module.css";
+// import AddGroupExpenseIncomeDialog from "../componets/AddGroupExpenseIncomeDialog";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const GroupListPage = () => {
-  const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "Name", headerName: "Name", width: 130 },
-    { field: "Description", headerName: "Description", width: 130 },
-  ];
-  const rows = [
-    {
-      id: 1,
-      Name: "Group1",
-      Description:
-        "A cost incurred necessary to conduct thecompletion of official State business.",
-    },
-    {
-      id: 2,
-      Name: "Group2",
-      Description:
-        "A cost incurred necessary to conduct thecompletion of official State business.",
-    },
-    {
-      id: 3,
-      Name: "Group3",
-      Description:
-        "A cost incurred necessary to conduct thecompletion of official State business.",
-    },
-    {
-      id: 3,
-      Name: "Group4",
-      Description:
-        "A cost incurred necessary to conduct thecompletion of official State business.",
-    },
-  ];
-  const [open, setOpen] = React.useState(false);
+  const client = axios.create({
+    baseURL: "https://localhost:3000",
+  });
+  const [expenseGroup, setExpenseGroup] = useState(null);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  useEffect(() => {
+    async function getExpenseGroup() {
+      const response = await axios.get(
+        "https://localhost:3000/exepense-groups"
+      );
+      setExpenseGroup(response.data);
+    }
+    getExpenseGroup();
+  }, []);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const columns = [
+  //   { field: "id", headerName: "ID", width: 70 },
+  //   { field: "Name", headerName: "Name", width: 130 },
+  //   { field: "Description", headerName: "Description", width: 130 },
+  // ];
+  // const rows = [
+  //   {
+  //     id: 1,
+  //     Name: "Group1",
+  //     Description:
+  //       "A cost incurred necessary to conduct thecompletion of official State business.",
+  //   },
+  //   {
+  //     id: 2,
+  //     Name: "Group2",
+  //     Description:
+  //       "A cost incurred necessary to conduct thecompletion of official State business.",
+  //   },
+  //   {
+  //     id: 3,
+  //     Name: "Group3",
+  //     Description:
+  //       "A cost incurred necessary to conduct thecompletion of official State business.",
+  //   },
+  //   {
+  //     id: 3,
+  //     Name: "Group4",
+  //     Description:
+  //       "A cost incurred necessary to conduct thecompletion of official State business.",
+  //   },
+  // ];
+
+  // const [open, setOpen] = React.useState(false);
+
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
   return (
-    <>
-      <MyTable rows={rows} columns={columns} title={"Expense Groups List"} />
+    // <>
+    <>{expenseGroup && <div>{expenseGroup}</div>}</>
+    /* <MyTable rows={rows} columns={columns} title={"Expense Groups List"} />
       <AddGroupExpenseIncomeDialog
         open={open}
         handleClose={handleClose}
@@ -83,8 +102,8 @@ const GroupListPage = () => {
         className={styles.button}
       >
         Edit income group
-      </Button>
-    </>
+      </Button> */
+    // </>
   );
 };
 
